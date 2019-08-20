e73b6e8a7d70307bde1b2a6a6ba9f43490c96d08
YukiSato55 <43591764+YukiSato55@users.noreply.github.com>
2019-08-20 16:25:08 +0900

    点滅処理

---
+++ Assets/Scripts/sample.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class sample : MonoBehaviour
+{
+
+	public Text text;
+	bool fe = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		Color color = text.material.GetColor("_Color");
+		//color.a = color.a <= 0 ? 1 : color.a - 0.01f;
+		if(fe == true)
+		{
+			color.a = color.a - 0.01f;
+			if(color.a <= 0.05)
+			{
+				fe = false;
+			}
+		}else if(fe == false)
+		{
+			color.a = color.a + 0.01f;
+			if (color.a >= 0.95)
+			{
+				fe = true;
+			}
+		}
+		text.material.SetColor("_Color", color);
+	}
+}
