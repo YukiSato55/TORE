9460612e2bd645763ca14d41116a6f693f8dcd52
RenIshizaki0819 <43591944+RenIshizaki0819@users.noreply.github.com>
2019-08-22 14:03:13 +0900

    8/22

---
+++ Assets/Scripts/ObjONOFF.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjONOFF : MonoBehaviour
+{
+    [SerializeField]
+    GameObject Obj;
+
+    public void ActiveON()
+    {
+        Obj.SetActive(true);
+    }
+
+    public void ActiveOFF()
+    {
+        Obj.SetActive(false);
+    }
+}
